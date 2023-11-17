3ca2a7d056c3d43cc7ca560feb916b611d5576dc
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-16 20:32:05 -0500

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [item for item in num_list if item%2==0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+   excl = [f'{item}!' for item in sentence_list]
+   return excl
